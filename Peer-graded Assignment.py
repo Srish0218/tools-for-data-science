d1cb058c9c7879c6bed6b7df6697593cd08a729e
Srishti Jaitly <87058044+Srish0218@users.noreply.github.com>
2023-06-23 17:39:55 +0530

    Add files via upload

---
+++ Peer-graded Assignment.py
@@ -0,0 +1,213 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Exercise 2 - Create a markdown cell with the title of the notebook. 
+
+
+# # ToolsForDataScience
+
+# In[ ]:
+
+
+Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+
+
+# This course introduced you to multiple data science tools, and in this final project, you will use Jupyterlite Notebook, one of the easiest tools to share publicly. 
+# 
+# Leveraging Jupyterlite Notebook on Skills Network labs, you will create your Jupyterlite Notebook (in English) and share it via a public GitHub link.
+# 
+# Review criteria
+# less 
+# There are a total of 25 points possible for this final project.   
+# 
+# Your grade will be based on the following Exercises:
+# 
+# Exercise 2 - Create a markdown cell with the title of the notebook. (1 pt)
+# 
+# Exercise 3 - Create a markdown cell for an introduction. (1 pt)
+# 
+# Exercise 4 - Create a markdown cell to list data science languages. (3 pts)
+# 
+# Exercise 5 - Create a markdown cell to list data science libraries. (3 pts)
+# 
+# Exercise 6 - Create a markdown cell with a table of Data Science tools. (3 pts)
+# 
+# Exercise 7 - Create a markdown cell introducing arithmetic expression examples. (1 pt)
+# 
+# Exercise 8 - Create a code cell to multiply and add numbers. (2 pts)
+# 
+# Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+# 
+# Exercise 10 -Insert a markdown cell to list Objectives. (3 pts)
+# 
+# Exercise 11 - Create a markdown cell to indicate the Author’s name. (2 pts)
+# 
+# Exercise 12 - Share your notebook through GitHub (3 pts)
+# 
+# Exercise 13 - Take a screenshot of the first page of the notebook. (1 pt)
+
+# In[ ]:
+
+
+Exercise 4 - Create a markdown cell to list data science languages. (3 pts)
+
+
+# Data Science Languages
+# 
+# Python
+# 
+# R
+# 
+# Julia
+# 
+# SQL
+# 
+# Scala
+# 
+# MATLAB
+# 
+# SAS
+# 
+# Java
+# C++
+# 
+# These languages are commonly used in the field of data science for tasks such as data analysis, machine learning, statistical modeling, and data visualization. Each language has its own strengths and features, and the choice of language often depends on the specific requirements and preferences of the data scientist or data analyst.
+# 
+
+# In[ ]:
+
+
+Exercise 5 - Create a markdown cell to list data science libraries. (3 pts)
+
+
+# Data Science Libraries
+# 
+# NumPy: A fundamental package for scientific computing with Python, providing powerful numerical operations and array manipulation capabilities.
+# 
+# Pandas: A library for data manipulation and analysis. It provides high-performance, easy-to-use data structures and data analysis tools.
+# 
+# Matplotlib: A plotting library for creating static, animated, and interactive visualizations in Python.
+# 
+# Scikit-learn: A machine learning library that provides a wide range of algorithms for classification, regression, clustering, and dimensionality reduction.
+# 
+# TensorFlow: An open-source library for machine learning and deep learning, known for its flexibility and scalability.
+# 
+# PyTorch: A deep learning framework that enables dynamic computational graphs and offers a seamless transition between training on CPUs and GPUs.
+# 
+# Keras: A high-level neural networks API that runs on top of TensorFlow, providing a user-friendly interface for building and training deep learning models.
+# 
+# SciPy: A library for scientific and technical computing in Python, offering modules for optimization, integration, interpolation, linear algebra, and more.
+# 
+# Seaborn: A data visualization library based on Matplotlib, providing a high-level interface for creating informative and attractive statistical graphics.
+# 
+# NLTK: The Natural Language Toolkit, which provides tools and resources for natural language processing tasks such as tokenization, stemming, tagging, and parsing.
+# 
+# These libraries are widely used in the field of data science to perform various tasks, including data manipulation, visualization, machine learning, deep learning, and natural language processing. Each library offers a range of functions and tools to streamline and enhance the data science workflow.
+
+# In[ ]:
+
+
+Exercise 6 - Create a markdown cell with a table of Data Science tools. (3 pts)
+
+
+# | Tool                 | Description                                                |
+# |----------------------|------------------------------------------------------------|
+# | Jupyter Notebook     | Interactive coding environment for data exploration         |
+# | Anaconda             | Distribution of Python and R packages for data science      |
+# | Apache Spark         | Distributed computing framework for big data processing     |
+# | Tableau              | Data visualization and business intelligence tool           |
+# | Apache Hadoop        | Distributed storage and processing framework for big data   |
+# | Git                  | Version control system for tracking code changes            |
+# | Docker               | Containerization platform for creating reproducible environments |
+# | TensorFlow           | Deep learning framework for building and training models    |
+# | PyCharm              | Integrated development environment (IDE) for Python         |
+# | RapidMiner           | Data mining and machine learning tool                       |
+# 
+
+# In[ ]:
+
+
+Exercise 7 - Create a markdown cell introducing arithmetic expression examples. (1 pt)
+
+
+# 
+# Arithmetic expressions are mathematical expressions that involve arithmetic operations such as addition, subtraction, multiplication, and division. They are commonly used in programming and mathematical calculations. Here are some examples of arithmetic expressions:
+# 
+# 1. Addition:
+#    - `3 + 5`: Evaluates to 8
+# 
+# 2. Subtraction:
+#    - `10 - 4`: Evaluates to 6
+# 
+# 3. Multiplication:
+#    - `6 * 4`: Evaluates to 24
+#    - `3.5 * 2.5`: Evaluates to 8.75
+# 
+# 4. Division:
+#    - `15 / 3`: Evaluates to 5
+#    - `10.5 / 2`: Evaluates to 5.25
+#     
+# These examples demonstrate basic arithmetic operations using both integers and floating-point numbers. Arithmetic expressions can also include parentheses to control the order of operations and complex mathematical functions for more advanced calculations. Understanding and working with arithmetic expressions is essential in various fields, including mathematics, computer science, and data analysis.
+# 
+
+# In[8]:
+
+
+# Arithmetic operations
+print("5+4: ")
+code = compile("5 + 4", "<string>", "eval")
+eval(code)
+
+
+
+# In[9]:
+
+
+import math
+# Volume of a sphere
+print("volumn of sphere")
+code2 = compile("\n4 / 3 * math.pi * math.pow(25, 3)", "<string>", "eval")
+eval(code2)
+
+
+# Exercise 8 - Create a code cell to multiply and add numbers.(2 pts)
+# 
+# This a simple arithmetic expression to mutiply then add integers
+# 
+
+# In[12]:
+
+
+(3*4)+5
+
+
+# Exercise 9 - Create a code cell to convert minutes to hours. (2 pts)
+# 
+# This will convert 200 minutes to hours by diving by 60
+
+# In[14]:
+
+
+min=200
+
+hours=200/60
+print(hours)
+
+
+# # Exercise 10 -Insert a markdown cell to list Objectives.
+# 
+# Below the introduction cell created in Exercise 3, insert a new markdown cell to list the objectives that this notebook covered (i.e. some of the key takeaways from the course). In this new cell start with an introductory line titled: Objectives: in bold font. Then using an unordered list (bullets) indicate 3 to 5 items covered in this notebook, such as List popular languages for Data Science.
+
+# # Exercise 11 - Create a markdown cell to indicate the Author’s name.
+# 
+# # Author:
+# Srishti Jaitly
+
+# In[ ]:
+
+
+
+
